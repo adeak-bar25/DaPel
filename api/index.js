87f9b5f731d8349c19a4ac62b75ab71d3c98e273
Ade Akbar Magri Dinata <160657955+adeak-bar25@users.noreply.github.com>
2025-08-22 16:20:25 +0800

    membuat vercel compatible

---
+++ api/index.js
@@ -0,0 +1,10 @@
+import app from '../app.js'
+// import { port } from '../utils/config';
+
+// const port = process.env.PORT || 3000;
+
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+export default app;
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import { port } from './utils/config.js';
+// import { port } from './utils/config.js';
 import express from 'express';
 import { connectToDB } from './utils/data/connection.js';
 import mainRouter from './routes/route.js';
-import { getAllStudentData } from './utils/data/data.js';
+// import { getAllStudentData } from './utils/data/data.js';
 
 connectToDB()
 
@@ -13,6 +13,8 @@ app.use(express.static('public'));
 
 app.use('/', mainRouter)
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+export default app;
