04ab3740f32f8c5af0b00d4ebb3e5ba31ba57a1c
Ade Akbar Magri Dinata <160657955+adeak-bar25@users.noreply.github.com>
2025-08-22 17:39:23 +0800

    membuat vercel compatible 2

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-// import { port } from './utils/config.js';
+import { port } from './utils/config.js';
 import express from 'express';
 import { connectToDB } from './utils/data/connection.js';
 import mainRouter from './routes/route.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 // import { getAllStudentData } from './utils/data/data.js';
 
 connectToDB()
 
 const app = express();
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static('public'));
 
 app.use('/', mainRouter)
+
+console.log('ENV:', process.env);
 
 // app.listen(port, () => {
 //   console.log(`Server is running on http://localhost:${port}`);
